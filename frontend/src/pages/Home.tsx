8c79feacc5550130a7729f79360dddb148b8781f
LucaslcFreitas <lucaslcfjf@hotmail.com>
2023-11-30 16:42:30 -0300

    Criação do segundo conteúdo da home

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,10 +1,12 @@
 import '../styles/pages/Home.sass';
-import Content1 from '../components/home/Context1';
+import Content1 from '../components/home/Content1';
+import Content2 from '../components/home/Content2';
 
 function Home() {
     return (
         <>
             <Content1 />
+            <Content2 />
         </>
     );
 }
