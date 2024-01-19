f53409afdaed92e2f5013ad4a56d6b76c1103182
LucaslcFreitas <lucaslcfjf@hotmail.com>
2024-01-19 19:00:45 -0300

    Remoção do Strict Mode

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 
@@ -8,9 +7,7 @@ import store from './redux/store.ts';
 import './styles/main.sass';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-    <React.StrictMode>
-        <Provider store={store}>
-            <App />
-        </Provider>
-    </React.StrictMode>
+    <Provider store={store}>
+        <App />
+    </Provider>
 );
